37293b8d5a5737dc838d2f0524fd8404652d177d
6e3dapb <kuu0000001@gmail.com>
2022-10-23 14:34:47 +0300

    client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,58 @@
-import express from "express"
+import express from 'express'
+import multer from 'multer'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import {registerValidation, loginValidation, postCreateValidation} from './validations.js'
+
+
+import { handleValidationErrors, checkAuth } from './utils/index.js';
+import {UserController, PostController} from './controllers/index.js'
+
+mongoose
+.connect('mongodb+srv://ritsch:kissxsis1313@cluster0.m8a0qvq.mongodb.net/web?retryWrites=true&w=majority')
+.then(()=>console.log('DB ok'))
+.catch((err)=>console.log('DB error',err))
 
 const app = express()
 
-app.get('/', (req,res)=>{
-    res.send()
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+      if (!fs.existsSync('uploads')) {
+        fs.mkdirSync('uploads')
+      }
+      cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+      cb(null, file.originalname)
+    },
+  })
+
+  const upload = multer({storage})
+
+app.use(express.json())
+app.use(cors())
+app.use('/uploads', express.static('uploads'))
+
+app.post('/auth/login',  loginValidation,handleValidationErrors, UserController.login)
+app.post('/auth/register',registerValidation,handleValidationErrors, UserController.registration)
+app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('/upload',checkAuth, upload.single('image'),(req,res)=>{
+    res.json({
+        url: `/uploads'/${req.file.originalname}`
+    })
 })
+
+app.get('/posts', PostController.getAll)
+app.get('/posts/tags', PostController.getLastTags)
+app.post('/posts',checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
+app.get('/posts/:id', PostController.getOne)
+app.delete('/posts/:id',checkAuth, PostController.remove)
+app.patch('/posts/:id',checkAuth,postCreateValidation,handleValidationErrors, PostController.update)
+
+app.listen(4444, (err)=>{
+    if (err){
+    return console.log(err)
+    }
+    console.log('Server is OK')
+}) 
---
+++ server/models/Post.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+    title:{type: String, required:true},
+    text:{type:String, required:true, unique:true},
+    tags:Array, default:[],
+    avatarUrl: String,
+    viewsCount:{type:Number, default: 0},
+    imageUrl:String,
+    user:{type:mongoose.Schema.Types.ObjectId, ref:'User', required:true},
+}, {
+    timestamps:true
+})
+
+export default mongoose.model('Post',PostSchema)
